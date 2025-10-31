74e7265bf24a3884d4d8e0ac3a190e682901e885
izzettinkarasayar <izzettin.karasayar@std.yildiz.edu.tr>
2025-10-31 16:11:49 +0300

    initial commit

---
+++ src/main/java/cmdotender/TaskLine/TaskLineApplication.java
@@ -0,0 +1,13 @@
+package cmdotender.TaskLine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskLineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskLineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cmdotender/TaskLine/TaskLineApplicationTests.java
@@ -0,0 +1,13 @@
+package cmdotender.TaskLine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskLineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
