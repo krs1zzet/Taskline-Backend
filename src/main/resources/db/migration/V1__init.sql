792fd7d1c1e45407d1ba3f204471ef3e0bd4367a
izzettinkarasayar <izzettin.karasayar@std.yildiz.edu.tr>
2025-11-10 07:03:00 +0300

    ``` chore(ci): update GitHub Actions workflow for conditional deployment
    - Modified the workflow to trigger builds and deployments only on the 'main' branch
    - Added conditions to push the 'latest' Docker image only when on the 'main' branch
    - Improved readability of branch specifications in the workflow configuration
    ```

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,4 +1,4 @@
-CREATE TABLE task (
+CREATE TABLE IF NOT EXIST task (
                       id            BIGSERIAL PRIMARY KEY,
                       title         VARCHAR(200) NOT NULL,
                       description   TEXT,
