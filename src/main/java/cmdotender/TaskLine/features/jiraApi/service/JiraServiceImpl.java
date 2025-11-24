73f435726103cff7ca0e8d3f46361e5b2c5951f9
izzettinkarasayar <izzettin.karasayar@std.yildiz.edu.tr>
2025-11-24 09:48:52 +0300

    ```markdown refactor(jiraApi): clean up imports in JiraServiceImpl
    - Removed unused imports related to AtlassianClient, AccessibleResourceDTO, UserDTO, User, UserService, ApiException, and ErrorCode
    - Cleaned up import statements to include only necessary classes
    - Improved code readability by reducing clutter in import section
    ```

--- src/main/java/cmdotender/TaskLine/features/jiraApi/service/JiraServiceImpl.java
+++ src/main/java/cmdotender/TaskLine/features/jiraApi/service/JiraServiceImpl.java
@@ -1,24 +1,12 @@
 package cmdotender.TaskLine.features.jiraApi.service;
 
-import cmdotender.TaskLine.features.auth.controller.AtlassianClient;
-import cmdotender.TaskLine.features.auth.controller.JiraApiClient;
-import cmdotender.TaskLine.features.auth.dto.AccessibleResourceDTO;
+import cmdotender.TaskLine.features.auth.service.JiraApiClient;
 import cmdotender.TaskLine.features.jiraApi.dto.JiraIssueLinkResponse;
 import cmdotender.TaskLine.features.jiraApi.dto.JiraIssueLinkTypeDTO;
-import cmdotender.TaskLine.features.user.dto.UserDTO;
-import cmdotender.TaskLine.features.user.entity.User;
-import cmdotender.TaskLine.features.user.service.UserService;
-import cmdotender.TaskLine.product.exceptions.customException.ApiException;
-import cmdotender.TaskLine.product.exceptions.errorCode.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.*;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
-import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
 import java.util.List;
 
 @Service
