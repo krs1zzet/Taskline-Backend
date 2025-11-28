5a1e2ff7ce70217ca5a7281291cf2db97d425339
izzettinkarasayar <izzettin.karasayar@std.yildiz.edu.tr>
2025-11-28 13:49:10 +0300

    feat(jira): implement issue picker service
    - Created `IssueServiceImpl` to handle issue picking functionality
    - Integrated `JiraApiClient` for communication with Jira API
    - Added method `IssuePicker` to retrieve issues based on a query
    - Utilized `UriComponentsBuilder` for building request URIs
    - Returned a list of `BaseIssueDTO` objects with issue details
    
    Refs: Default

---
+++ src/main/java/cmdotender/TaskLine/features/jira/issue/service/Impl/IssueServiceImpl.java
@@ -0,0 +1,50 @@
+package cmdotender.TaskLine.features.jira.issue.service.Impl;
+
+import cmdotender.TaskLine.features.auth.service.AtlassianClient;
+import cmdotender.TaskLine.features.auth.service.JiraApiClient;
+import cmdotender.TaskLine.features.jira.issue.dto.BaseIssueDTO;
+import cmdotender.TaskLine.features.jira.issue.dto.IssuePickerResponseDTO;
+import cmdotender.TaskLine.features.jira.issue.service.IssueService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class IssueServiceImpl implements IssueService {
+
+    private final JiraApiClient jiraApiClient;
+
+    @Override
+    public List<BaseIssueDTO> IssuePicker(String query) {
+
+        String path = UriComponentsBuilder
+                .fromPath("/issue/picker")
+                .queryParam("query", query)
+                .build()
+                .encode()
+                .toUriString();
+
+        IssuePickerResponseDTO response = jiraApiClient.get(path, IssuePickerResponseDTO.class);
+
+        if (response.getSections() == null) return List.of();
+
+        return response.getSections().stream()
+                .filter(s -> s.getIssues() != null)
+                .flatMap(s -> s.getIssues().stream())
+                .map(i -> new BaseIssueDTO(
+                        i.getId(),
+                        i.getKey(),
+                        (i.getSummaryText() != null && !i.getSummaryText().isBlank())
+                                ? i.getSummaryText()
+                                : i.getSummary()
+                ))
+                .toList();
+    }
+
+
+}
